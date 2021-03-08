115834ab9a9a8e175a93f6b09431859916fecc4a
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-03-08 15:18:19 +0530

    Start Working

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>This is for Practicing Functional Programming Again</h1>
+      <h2>By the way this is my third time!</h2>
     </div>
   );
 }
